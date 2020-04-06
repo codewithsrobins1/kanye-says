c01814a20bb50a74e661f04394f3fa7dfe6b6160
unknown <codewithsrobins1@gmail.com>
2020-04-05 23:16:03 -0700

    Updates

---
+++ precache-manifest.da1dd8ea27f873c9ae0e607a2ce13739.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f12aaed520bcad70540fa7bacc45f075",
+    "url": "/kanye-says/index.html"
+  },
+  {
+    "revision": "13bdf5970a12396c59c7",
+    "url": "/kanye-says/static/css/main.8f55968f.chunk.css"
+  },
+  {
+    "revision": "86cc5c0dc2a81863e3bf",
+    "url": "/kanye-says/static/js/2.4b1c1a8e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kanye-says/static/js/2.4b1c1a8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13bdf5970a12396c59c7",
+    "url": "/kanye-says/static/js/main.8b9a47eb.chunk.js"
+  },
+  {
+    "revision": "ca111c9480074327bb8b",
+    "url": "/kanye-says/static/js/runtime-main.3fcf804d.js"
+  }
+]);
